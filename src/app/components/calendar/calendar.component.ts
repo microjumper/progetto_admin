d62715ddec5234ed9a60c7175cc712b4df05fac8
Salvatore Sammarco <microjmpr@gmail.com>
2024-05-18 20:12:07 +0200

    refactoring

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectorRef, Component, signal} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import { FullCalendarModule } from "@fullcalendar/angular";
 import { CalendarOptions, DateSelectArg, EventApi, EventClickArg } from "@fullcalendar/core";
@@ -6,17 +6,15 @@ import { CalendarOptions, DateSelectArg, EventApi, EventClickArg } from "@fullca
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list';
-import interactionPlugin, {Draggable} from '@fullcalendar/interaction';
+import interactionPlugin, { Draggable, EventReceiveArg } from '@fullcalendar/interaction';
 
 import itLocale from '@fullcalendar/core/locales/it';
-import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
   imports: [
-    FullCalendarModule,
-    NgForOf
+    FullCalendarModule
   ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
@@ -25,25 +23,13 @@ export class CalendarComponent implements AfterViewInit{
 
   calendarOptions: CalendarOptions | undefined;
 
-  constructor(private changeDetector: ChangeDetectorRef) {
+  constructor() {
     this.initCalendar();
   }
 
   ngAfterViewInit(): void {
-    const externalEventsElement = document.getElementById('external-events');
-    if (externalEventsElement) {
-      const externalEvents = new Draggable(externalEventsElement, {
-        itemSelector: '.fc-event',
-        eventData: function(eventEl) {
-          return {
-            title: eventEl.innerText.trim()
-          };
-        }
-      });
-    } else {
-      console.error("External events container not found.");
-    }
-    }
+    this.initExternalEventsDraggable();
+  }
 
   private initCalendar(): void {
     this.calendarOptions = {
@@ -72,20 +58,32 @@ export class CalendarComponent implements AfterViewInit{
           duration: { weeks: 1 }
         }
       },
-      select: this.handleDateSelect.bind(this),
-      eventClick: this.handleEventClick.bind(this),
-      eventsSet: this.handleEvents.bind(this),
-      eventReceive: (info) => {
-        // Handle event dropping here, for example, add the dropped event to the calendar
-        console.log("event receinved")
-      },
-      drop: (info) => {
-        console.log('Dropped event:', info);
-      }
+      select: selectInfo => this.handleSelect(selectInfo),
+      eventClick: clickInfo => this.handleClick(clickInfo),
+      eventsSet: events => this.handleSet(events),
+      eventReceive: info => this.handleReceive(info),
+      drop: dropArg => console.log(dropArg)
     };
   }
 
-  private handleDateSelect(selectInfo: DateSelectArg) {
+  private initExternalEventsDraggable(): void {
+    const externalEventsElement = document.getElementById('external-events');
+    if (externalEventsElement) {
+      const externalEvents = new Draggable(externalEventsElement, {
+        itemSelector: '.fc-event',
+        eventData: (eventEl) => {
+          const title = eventEl.innerText.trim();
+          console.info(`Dragging ${title}`);
+          return { title };
+        }
+      });
+    }
+    else {
+      console.error("External events container not found.");
+    }
+  }
+
+  private handleSelect(selectInfo: DateSelectArg) {
     const title = prompt('Please enter a new title for your event');
     const calendarApi = selectInfo.view.calendar;
 
@@ -102,11 +100,16 @@ export class CalendarComponent implements AfterViewInit{
     }
   }
 
-  private handleEventClick(clickInfo: EventClickArg) {
+  private handleClick(clickInfo: EventClickArg) {
     console.log(clickInfo.event);
   }
 
-  private handleEvents(events: EventApi[]) {
+  private handleSet(events: EventApi[]) {
     console.log(events);
+  }
+
+  private handleReceive(info: EventReceiveArg): void
+  {
+    console.log(info)
   }
 }
